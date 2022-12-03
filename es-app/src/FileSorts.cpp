81538b9bbc4d93731a43a45a652961402de62202
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2022-12-03 16:15:51 +0000

    Update FileSorts.cpp

--- es-app/src/FileSorts.cpp
+++ es-app/src/FileSorts.cpp
@@ -8,35 +8,32 @@ namespace FileSorts
 {
 
 	const FileData::SortType typesArr[] = {
-		FileData::SortType(&compareName, true, "filename, ascending"),
-		FileData::SortType(&compareName, false, "filename, descending"),
+		FileData::SortType(&compareName, true, "name"),
+		FileData::SortType(&compareName, false, "name / reverse"),
 
-		FileData::SortType(&compareRating, true, "rating, ascending"),
-		FileData::SortType(&compareRating, false, "rating, descending"),
+		FileData::SortType(&compareFullSystem, true, "system"),
+		FileData::SortType(&compareFullSystem, false, "system / reverse"),
 
-		FileData::SortType(&compareTimesPlayed, true, "times played, ascending"),
-		FileData::SortType(&compareTimesPlayed, false, "times played, descending"),
+		FileData::SortType(&comparePublisher, true, "publisher"),
+		FileData::SortType(&comparePublisher, false, "publisher / reverse"),
 
-		FileData::SortType(&compareLastPlayed, true, "last played, ascending"),
-		FileData::SortType(&compareLastPlayed, false, "last played, descending"),
+		FileData::SortType(&compareReleaseDate, true, "release date"),
+		FileData::SortType(&compareReleaseDate, false, "release date / reverse"),
 
-		FileData::SortType(&compareNumPlayers, true, "number players, ascending"),
-		FileData::SortType(&compareNumPlayers, false, "number players, descending"),
+		FileData::SortType(&compareGenre, true, "genre"),
+		FileData::SortType(&compareGenre, false, "genre / reverse"),
 
-		FileData::SortType(&compareReleaseDate, true, "release date, ascending"),
-		FileData::SortType(&compareReleaseDate, false, "release date, descending"),
+		FileData::SortType(&compareNumPlayers, true, "players"),
+		FileData::SortType(&compareNumPlayers, false, "players / reverse"),
 
-		FileData::SortType(&compareGenre, true, "genre, ascending"),
-		FileData::SortType(&compareGenre, false, "genre, descending"),
+		FileData::SortType(&compareRegion, true, "region"),
+		FileData::SortType(&compareRegion, false, "region / reverse"),
 
-		FileData::SortType(&compareDeveloper, true, "developer, ascending"),
-		FileData::SortType(&compareDeveloper, false, "developer, descending"),
+		FileData::SortType(&compareTimesPlayed, false, "most played"),
+		FileData::SortType(&compareTimesPlayed, true, "most played / reverse"),
 
-		FileData::SortType(&comparePublisher, true, "publisher, ascending"),
-		FileData::SortType(&comparePublisher, false, "publisher, descending"),
-
-		FileData::SortType(&compareSystem, true, "system, ascending"),
-		FileData::SortType(&compareSystem, false, "system, descending")
+		FileData::SortType(&compareLastPlayed, false, "last played"),
+		FileData::SortType(&compareLastPlayed, true, "last played / reverse"),
 	};
 
 	const std::vector<FileData::SortType> SortTypes(typesArr, typesArr + sizeof(typesArr)/sizeof(typesArr[0]));
@@ -59,32 +56,18 @@ namespace FileSorts
 		return name1.compare(name2) < 0;
 	}
 
-	bool compareRating(const FileData* file1, const FileData* file2)
+	bool compareFullSystem(const FileData* file1, const FileData* file2)
 	{
-		return file1->metadata.getFloat("rating") < file2->metadata.getFloat("rating");
+		std::string fullsystem1 = Utils::String::toUpper(file1->metadata.get("fullsystem"));
+		std::string fullsystem2 = Utils::String::toUpper(file2->metadata.get("fullsystem"));
+		return fullsystem1.compare(fullsystem2) < 0;
 	}
 
-	bool compareTimesPlayed(const FileData* file1, const FileData* file2)
+	bool comparePublisher(const FileData* file1, const FileData* file2)
 	{
-		//only games have playcount metadata
-		if(file1->metadata.getType() == GAME_METADATA && file2->metadata.getType() == GAME_METADATA)
-		{
-			return (file1)->metadata.getInt("playcount") < (file2)->metadata.getInt("playcount");
-		}
-
-		return false;
-	}
-
-	bool compareLastPlayed(const FileData* file1, const FileData* file2)
-	{
-		// since it's stored as an ISO string (YYYYMMDDTHHMMSS), we can compare as a string
-		// as it's a lot faster than the time casts and then time comparisons
-		return (file1)->metadata.get("lastplayed") < (file2)->metadata.get("lastplayed");
-	}
-
-	bool compareNumPlayers(const FileData* file1, const FileData* file2)
-	{
-		return (file1)->metadata.getInt("players") < (file2)->metadata.getInt("players");
+		std::string publisher1 = Utils::String::toUpper(file1->metadata.get("publisher"));
+		std::string publisher2 = Utils::String::toUpper(file2->metadata.get("publisher"));
+		return publisher1.compare(publisher2) < 0;
 	}
 
 	bool compareReleaseDate(const FileData* file1, const FileData* file2)
@@ -101,25 +84,36 @@ namespace FileSorts
 		return genre1.compare(genre2) < 0;
 	}
 
-	bool compareDeveloper(const FileData* file1, const FileData* file2)
+	bool compareNumPlayers(const FileData* file1, const FileData* file2)
 	{
-		std::string developer1 = Utils::String::toUpper(file1->metadata.get("developer"));
-		std::string developer2 = Utils::String::toUpper(file2->metadata.get("developer"));
-		return developer1.compare(developer2) < 0;
+		std::string players1 = Utils::String::toUpper(file1->metadata.get("players"));
+		std::string players2 = Utils::String::toUpper(file2->metadata.get("players"));
+		return players1.compare(players2) < 0;
 	}
 
-	bool comparePublisher(const FileData* file1, const FileData* file2)
+	bool compareRegion(const FileData* file1, const FileData* file2)
 	{
-		std::string publisher1 = Utils::String::toUpper(file1->metadata.get("publisher"));
-		std::string publisher2 = Utils::String::toUpper(file2->metadata.get("publisher"));
-		return publisher1.compare(publisher2) < 0;
+		std::string region1 = Utils::String::toUpper(file1->metadata.get("region"));
+		std::string region2 = Utils::String::toUpper(file2->metadata.get("region"));
+		return region1.compare(region2) < 0;
+	}
+	
+	bool compareTimesPlayed(const FileData* file1, const FileData* file2)
+	{
+		//only games have playcount metadata
+		if(file1->metadata.getType() == GAME_METADATA && file2->metadata.getType() == GAME_METADATA)
+		{
+			return (file1)->metadata.getInt("playcount") < (file2)->metadata.getInt("playcount");
+		}
+		
+		return false;
 	}
 
-	bool compareSystem(const FileData* file1, const FileData* file2)
+	bool compareLastPlayed(const FileData* file1, const FileData* file2)
 	{
-		std::string system1 = Utils::String::toUpper(file1->getSystemName());
-		std::string system2 = Utils::String::toUpper(file2->getSystemName());
-		return system1.compare(system2) < 0;
+		// since it's stored as an ISO string (YYYYMMDDTHHMMSS), we can compare as a string
+		// as it's a lot faster than the time casts and then time comparisons
+		return (file1)->metadata.get("lastplayed") < (file2)->metadata.get("lastplayed");
 	}
 
 	//If option is enabled, ignore leading articles by temporarily modifying the name prior to sorting
