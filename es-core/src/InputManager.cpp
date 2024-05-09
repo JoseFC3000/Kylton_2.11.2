2aa0a139535901e65c2b2416062aa3d6e8dd892c
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-05-09 22:03:10 +0100

    Update InputManager.cpp

--- es-core/src/InputManager.cpp
+++ es-core/src/InputManager.cpp
@@ -371,6 +371,7 @@ void InputManager::writeDeviceConfig(InputConfig* config)
 					path = getTemporaryConfigPath();
 					doc.reset();
 					root = doc.append_child("inputList");
+					root.append_copy(actionnode);
 				}
 				else
 				{
