a2f3e1cb62376227faf3b6f5fae1a50926ebb74b
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2024-07-16 03:57:54 +0100

    Update Font.h

--- es-core/src/resources/Font.h
+++ es-core/src/resources/Font.h
@@ -13,7 +13,6 @@
 
 class TextCache;
 
-#define FONT_SIZE_HELP ((unsigned int)(0.025f * Math::min((int)Renderer::getScreenHeight(), (int)Renderer::getScreenWidth())))
 #define FONT_SIZE_MINI ((unsigned int)(0.020f * Math::min((int)Renderer::getScreenHeight(), (int)Renderer::getScreenWidth())))
 #define FONT_SIZE_SMALL ((unsigned int)(0.025f * Math::min((int)Renderer::getScreenHeight(), (int)Renderer::getScreenWidth())))
 #define FONT_SIZE_MEDIUM ((unsigned int)(0.035f * Math::min((int)Renderer::getScreenHeight(), (int)Renderer::getScreenWidth())))
